c63881cc054936e26b5e376b295f573fe3dbcbed
milt7oner <duvans427@gmail.com>
2025-09-27 15:03:22 -0500

    Subimos api para producion

--- routes/crop_router.py
+++ routes/crop_router.py
@@ -6,19 +6,17 @@ router = APIRouter()
 
 @router.post("/predict")
 async def cropPredict(data: CropData, model: str = "rf"):
-    # Llamamos a la función de predicción
+
     prediction = crop_prediction(data, model_name=model)
 
-    # Si la predicción viene como numpy array → convertir
     if hasattr(prediction, "tolist"):
         prediction = prediction.tolist()
 
-    # Si es lista/array → tomar primer valor
+
     if isinstance(prediction, (list, tuple)):
         if len(prediction) == 1:
             prediction = prediction[0]
 
-    # Asegurar que sea string (ejemplo: "maíz", "rice")
     prediction = str(prediction)
 
     return {
